b8177ffda3fdac0d62f47aebf4bff34c781cde15
gurkay <g_basyigit@hotmail.com>
2021-10-23 13:32:23 +0300

    new codes

--- lib/screens/screen_home_page.dart
+++ lib/screens/screen_home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+
+import '../widgets/screen_home_page_drawer.dart';
+import '../widgets/screen_home_page_body.dart';
 
 class ScreenHomePage extends StatefulWidget {
   const ScreenHomePage({Key? key}) : super(key: key);
@@ -9,100 +11,14 @@ class ScreenHomePage extends StatefulWidget {
 }
 
 class _ScreenHomePageState extends State<ScreenHomePage> {
-  int _selectedDestination = 0;
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    final textTheme = theme.textTheme;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Screen Home Page'),
       ),
-      drawer: SafeArea(
-        child: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Center(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 5.0),
-                      child: CircleAvatar(
-                        child: Image.asset('assets/icons/halim_logo.png'),
-                        radius: MediaQuery.of(context).size.height * 0.090,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                    child: Column(
-                      children: [
-                        Text(
-                          'Header',
-                          style: textTheme.headline6,
-                        ),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 16.0, top: 2.0),
-                    child: Text(
-                      'e-mail@gmail.com',
-                      style: GoogleFonts.abhayaLibre(fontSize: 12),
-                    ),
-                  ),
-                ],
-              ),
-              const Divider(
-                height: 1,
-                thickness: 1,
-              ),
-              ListTile(
-                leading: const Icon(Icons.favorite),
-                title: const Text('Item 1'),
-                selected: _selectedDestination == 0,
-                onTap: () => selectDestination(0),
-              ),
-              ListTile(
-                leading: const Icon(Icons.delete),
-                title: const Text('Item 2'),
-                selected: _selectedDestination == 1,
-                onTap: () => selectDestination(1),
-              ),
-              ListTile(
-                leading: const Icon(Icons.label),
-                title: const Text('Item 3'),
-                selected: _selectedDestination == 2,
-                onTap: () => selectDestination(2),
-              ),
-              const Divider(
-                height: 1,
-                thickness: 1,
-              ),
-              const Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Text('Label'),
-              ),
-              ListTile(
-                leading: const Icon(Icons.bookmark),
-                title: const Text('Item A'),
-                selected: _selectedDestination == 3,
-                onTap: () => selectDestination(3),
-              ),
-            ],
-          ),
-        ),
-      ),
-      body: const Text('data'),
+      drawer: ScreenHomePageDrawer(),
+      body: ScreenHomePageBody(),
     );
-  }
-
-  void selectDestination(int index) {
-    setState(() {
-      _selectedDestination = index;
-      print('_selectedDestination::: ${_selectedDestination}');
-    });
   }
 }
---
+++ lib/widgets/screen_home_page_body.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ScreenHomePageBody extends StatefulWidget {
+  const ScreenHomePageBody({Key? key}) : super(key: key);
+
+  @override
+  _ScreenHomePageBodyState createState() => _ScreenHomePageBodyState();
+}
+
+class _ScreenHomePageBodyState extends State<ScreenHomePageBody> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('test'),
+    );
+  }
+}
