00f3c455b9834dbcc2824d42651382ade81a5d1a
gurkay <g_basyigit@hotmail.com>
2021-10-27 21:10:01 +0300

    new codes

--- lib/screens/screen_question_app.dart
+++ lib/screens/screen_question_app.dart
@@ -11,14 +11,76 @@ class _ScreenQuestionAppState extends State<ScreenQuestionApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.indigo[700],
       appBar: AppBar(
-        title: Text('dsfsf'),
+        title: Text('Question App'),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Text('data'),
+          Expanded(
+            flex: 4,
+            child: Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Center(
+                child: Text(
+                  'Question of info test',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 6.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.red[400],
+                        ),
+                        child: Container(
+                          padding: EdgeInsets.all(12.0),
+                          child: Icon(
+                            Icons.thumb_down,
+                            size: 30.0,
+                          ),
+                        ),
+                        onPressed: () {},
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.green[400],
+                        ),
+                        onPressed: () {},
+                        child: Container(
+                          padding: EdgeInsets.all(12.0),
+                          child: Icon(
+                            Icons.thumb_up,
+                            size: 30.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
